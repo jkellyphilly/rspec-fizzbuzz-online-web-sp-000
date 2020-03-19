8b3843c130fa692444f46d26afbd20a4e20ca746
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-19 15:48:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(input)
   if input % 3 == 0 && input % 5 == 0
-    puts "FizzBuzz"
+    return "FizzBuzz"
   elsif input % 3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif input % 5 == 0
-    puts "Buzz"
+    return "Buzz"
   end
 end
